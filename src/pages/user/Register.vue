97545c42b9fe4e09e0cc3efaa9b6f4427121b947
Ljw <871460573@qq.com>
2019-12-03 22:23:40 +0800

    2019-12-03 10：22更新内容： 1、更新pandalogo.png显示错误；

--- src/pages/user/Register.vue
+++ src/pages/user/Register.vue
@@ -8,7 +8,10 @@
                     <div class="grid-content bg-purple-light">
 
                         <center>
-                            <h1>注册</h1>
+                            <div>
+                                <img src="../../assets/pandalogo.png">
+                                <h1>注册</h1>
+                            </div>
                             <br>
 
 
@@ -20,14 +23,14 @@
                                     label-width="140px"
                                     class="demo-ruleForm m-top-60px">
                                 <el-form-item label="帐号" class="">
-                                    <el-input class="float-left width-60" v-model="ruleForm.username">
+                                    <el-input class="float-left width-60" v-model="ruleForm.username" placeholder="请输入账号">
 
                                     </el-input>
                                 </el-form-item>
 
                                 <el-form-item label="密码" prop="pass">
                                     <el-input class="float-left width-60" type="password"
-                                              v-model="ruleForm.password" autocomplete="off">
+                                              v-model="ruleForm.password" autocomplete="off" placeholder="请输入6位以上密码">
                                     </el-input>
                                 </el-form-item>
 
